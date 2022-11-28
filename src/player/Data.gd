b505f701475545395f27164ab0636d7acbea0c5c
Ryn <94999239+Ryn503@users.noreply.github.com>
2022-11-28 19:55:02 -0300

    Add files via upload

---
+++ src/player/Data.gd
@@ -0,0 +1,206 @@
+extends Node
+
+var nickname := "Benjamin"
+var playing := false
+var playtime := 0.0
+var volume := 10
+var char_per_seconds := 40
+var language := -1
+var last_position := Vector2.ZERO
+var last_direction := Vector2.ZERO
+var current_location := 0
+var items := []
+var unlocked_doors := []
+var first_time := true
+var first_chase := false
+var first_wardrobe_interaction := false
+var last_teleporter := Vector2.ZERO
+var utility_room_unlocked := false
+var deposit_lever_1_enabled := false
+var deposit_lever_2_enabled := false
+var deposit_lever_3_enabled := false
+var deposit_lever_4_enabled := false
+var deposit_lever_5_enabled := false
+var deposit_lever_6_enabled := false
+var deposit_lever_7_enabled := false
+var deposit_lever_8_enabled := false
+var basement_safer_unlocked := false
+var cleaning_room_unlocked := false
+var dining_room_unlocked := false
+var floor_2_unlocked := false
+var safe_box_unlocked := false
+var hallway_unlocked := false
+var lever_inside_playroom_enabled := false
+var cell_unlocked := false
+var lever_inside_cell_enabled := false
+var dorm_4_unlocked := false
+var dorm_5_unlocked := false
+var dorm_6_unlocked := false
+var dorm_7_unlocked := false
+var old_room_unlocked := false
+var valve_wheel_enabled := false
+var restroom_unlocked := false
+var bathroom_unlocked := false
+var klishe_room_unlocked := false
+
+
+func _unhandled_input(event : InputEvent):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_F11:
+			OS.window_fullscreen = not OS.window_fullscreen
+
+
+func _process(delta : float):
+	if not playing : return
+	
+	playtime += delta
+
+
+func save_config() -> void:
+	var file := File.new()
+	
+	file.open_compressed("user://config.dat", File.WRITE, File.COMPRESSION_DEFLATE)
+	file.store_32(volume)
+	file.store_32(char_per_seconds)
+	file.store_64(language)
+	file.close()
+
+
+func load_config() -> void:
+	var file := File.new()
+	var error := file.open_compressed("user://config.dat", File.READ, File.COMPRESSION_DEFLATE)
+	
+	if error != OK : return
+	
+	volume = file.get_32()
+	char_per_seconds = file.get_32()
+	language = file.get_64()
+	file.close()
+
+
+func save_data(slot : int, texture : Image) -> void:
+	var file := File.new()
+	
+	file.open_compressed("user://saves/slot%d/data.dat" % slot, File.WRITE, File.COMPRESSION_DEFLATE)
+	file.store_32(current_location)
+	file.store_float(playtime)
+	file.store_pascal_string(nickname)
+	
+	if get_meta("from_pos", true):
+		last_position = get_meta("scene_pos")
+		last_direction = get_meta("scene_look")
+	
+	file.store_float(last_position.x)
+	file.store_float(last_position.y)
+	file.store_float(last_direction.x)
+	file.store_float(last_direction.y)
+	file.store_32(items.size())
+	for i in items.size() : file.store_32(items[i])
+	file.store_32(unlocked_doors.size())
+	for i in unlocked_doors.size() : file.store_32(unlocked_doors[i])
+	file.store_32(int(first_time))
+	file.store_32(int(first_chase))
+	file.store_32(int(first_wardrobe_interaction))
+	file.store_float(last_teleporter.x)
+	file.store_float(last_teleporter.y)
+	file.store_32(int(utility_room_unlocked))
+	file.store_32(int(deposit_lever_1_enabled))
+	file.store_32(int(deposit_lever_2_enabled))
+	file.store_32(int(deposit_lever_3_enabled))
+	file.store_32(int(deposit_lever_4_enabled))
+	file.store_32(int(deposit_lever_5_enabled))
+	file.store_32(int(deposit_lever_6_enabled))
+	file.store_32(int(deposit_lever_7_enabled))
+	file.store_32(int(deposit_lever_8_enabled))
+	file.store_32(int(basement_safer_unlocked))
+	file.store_32(int(cleaning_room_unlocked))
+	file.store_32(int(dining_room_unlocked))
+	file.store_32(int(floor_2_unlocked))
+	file.store_32(int(safe_box_unlocked))
+	file.store_32(int(hallway_unlocked))
+	file.store_32(int(lever_inside_playroom_enabled))
+	file.store_32(int(cell_unlocked))
+	file.store_32(int(lever_inside_cell_enabled))
+	file.store_32(int(dorm_4_unlocked))
+	file.store_32(int(dorm_5_unlocked))
+	file.store_32(int(dorm_6_unlocked))
+	file.store_32(int(dorm_7_unlocked))
+	file.store_32(int(old_room_unlocked))
+	file.store_32(int(valve_wheel_enabled))
+	file.store_32(int(restroom_unlocked))
+	file.store_32(int(bathroom_unlocked))
+	file.store_32(int(klishe_room_unlocked))
+	
+	texture.save_png("user://saves/slot%d/icon.png" % slot)
+	file.close()
+
+
+func load_data(slot : int) -> bool:
+	var file := File.new()
+	var error := file.open_compressed("user://saves/slot%d/data.dat" % slot, File.READ, File.COMPRESSION_DEFLATE)
+	
+	if error != OK : return false
+	
+	items.clear()
+	unlocked_doors.clear()
+	
+	current_location = file.get_32()
+	playtime = file.get_float()
+	nickname = file.get_pascal_string()
+	last_position = Vector2(file.get_float(), file.get_float())
+	last_direction = Vector2(file.get_float(), file.get_float())
+	for i in file.get_32() : items.append(file.get_32())
+	for i in file.get_32() : unlocked_doors.append(file.get_32())
+	first_time = bool(file.get_32())
+	first_chase = bool(file.get_32())
+	first_wardrobe_interaction = bool(file.get_32())
+	last_teleporter = Vector2(file.get_float(), file.get_float())
+	utility_room_unlocked = bool(file.get_32())
+	deposit_lever_1_enabled = bool(file.get_32())
+	deposit_lever_2_enabled = bool(file.get_32())
+	deposit_lever_3_enabled = bool(file.get_32())
+	deposit_lever_4_enabled = bool(file.get_32())
+	deposit_lever_5_enabled = bool(file.get_32())
+	deposit_lever_6_enabled = bool(file.get_32())
+	deposit_lever_7_enabled = bool(file.get_32())
+	deposit_lever_8_enabled = bool(file.get_32())
+	basement_safer_unlocked = bool(file.get_32())
+	cleaning_room_unlocked = bool(file.get_32())
+	dining_room_unlocked = bool(file.get_32())
+	floor_2_unlocked = bool(file.get_32())
+	safe_box_unlocked = bool(file.get_32())
+	hallway_unlocked = bool(file.get_32())
+	lever_inside_playroom_enabled = bool(file.get_32())
+	cell_unlocked = bool(file.get_32())
+	lever_inside_cell_enabled = bool(file.get_32())
+	dorm_4_unlocked = bool(file.get_32())
+	dorm_5_unlocked = bool(file.get_32())
+	dorm_6_unlocked = bool(file.get_32())
+	dorm_7_unlocked = bool(file.get_32())
+	old_room_unlocked = bool(file.get_32())
+	valve_wheel_enabled = bool(file.get_32())
+	restroom_unlocked = bool(file.get_32())
+	bathroom_unlocked = bool(file.get_32())
+	klishe_room_unlocked = bool(file.get_32())
+	
+	file.close()
+	return true
+
+
+func _ready():
+	load_config()
+	
+	if Data.language == -1:
+		Data.language = Globals.string_to_language(OS.get_locale())
+		
+		if Data.language == -1:
+			TranslationServer.set_locale("en_US")
+			Data.language = Globals.Languages.EN_US
+			
+		else : TranslationServer.set_locale(Globals.language_to_string(Data.language))
+			
+	else:
+		TranslationServer.set_locale(Globals.language_to_string(Data.language))
+		Data.language = Globals.string_to_language(TranslationServer.get_locale())
+		
+	#Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
---
+++ src/player/Player.gd
@@ -0,0 +1,92 @@
+extends Area2D
+
+onready var _tween : Tween = $Tween
+onready var _ray : RayCast2D = $RayCast2D
+onready var _anim_player : AnimationPlayer = $AnimationPlayer
+onready var _grid : TileMap = get_parent().get_node("Solid")
+onready var _ui : CanvasLayer = get_parent().get_node("UI")
+onready var _light : Light2D = $Light2D
+
+signal open_inventory
+signal open_menu
+
+const WALK_SPEED := 1.0
+const SPRINT_SPEED := 2.0
+
+var warping := false
+
+
+func _process(_delta : float):
+	var move := Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).ceil()
+	
+	if Data.first_time : return
+	
+	if Input.is_action_just_pressed("open_inventory") and not Data.get_meta("chased", false):
+		emit_signal("open_inventory")
+		set_process(false)
+	
+	if Input.is_action_just_pressed("open_menu") and not Data.get_meta("chased", false):
+		emit_signal("open_menu")
+		set_process(false)
+	
+	if _ray.is_colliding() and Input.is_action_just_pressed("ui_accept"):
+		var obj : Node2D = _ray.get_collider()
+		
+		if obj : if obj.has_method("action"):
+			obj.action(self)
+	
+	if not move or warping: return
+	if move.length() > 1 : move.x = 0
+	
+	_anim_player.playback_speed = SPRINT_SPEED if Input.is_action_pressed("ui_cancel") else WALK_SPEED
+	
+	var dir : Vector2 = _grid.request_movement(self, move)
+	
+	if dir:
+		set_direction(move, false)
+		move_to_grid(dir)
+		return
+		
+	set_direction(move, true)
+
+
+func set_direction(direction : Vector2, idle : bool) -> void:
+	Data.last_direction = direction
+	_ray.rotation = direction.angle()
+	
+	match direction:
+		Vector2.UP : _anim_player.play("idle_up" if idle else "walk_up")
+		Vector2.LEFT : _anim_player.play("idle_left" if idle else "walk_left")
+		Vector2.DOWN : _anim_player.play("idle_down" if idle else "walk_down")
+		Vector2.RIGHT : _anim_player.play("idle_right" if idle else "walk_right")
+
+
+func move_to_grid(target : Vector2) -> void:
+	set_process(false)
+	
+	_tween.interpolate_property(
+		self, "position", position, target,
+		_anim_player.current_animation_length / _anim_player.playback_speed,
+		Tween.TRANS_LINEAR, Tween.EASE_IN
+	)
+	_tween.start()
+	
+	yield(_anim_player, "animation_finished")
+	set_process(true)
+	Data.last_position = global_position
+
+
+func _ready():
+	var inventory := _ui.get_node("Inventory")
+	var menu := _ui.get_node("Menu")
+	
+	inventory.player = self
+	menu.player = self
+	
+	connect("open_inventory", inventory, "_open")
+	connect("open_menu", menu, "_open")
+	
+	global_position = _grid.map_to_world(_grid.world_to_map(global_position)) + _grid.cell_size / 2
